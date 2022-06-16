4d44071553abfc8a29caaaaef07a885f3e97b7fb
ec1086 <ec1086@gmail.com>
2022-06-16 00:17:40 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// const numberArray = ["1", "2", "3", "4", "5"];
+// const suitArray = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+// function random(array) {
+//   return Math.floor(Math.random() * array.length);
+// }
 
+//const numberIndex = random(numberArray);
+//const suitIndex = random(suitArray);
+// const numberValue = numberArray[numberIndex];
+// const numberValue = numberArray[random(numberArray)];
+// const suitValue = suitArray[random(suitArray)];
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //BigFunc();
+  // document.querySelector(".number").innerHTML = numberValue;
+  // document.querySelector(".top").innerHTML = suitValue;
+  // document.querySelector(".bottom").innerHTML = suitValue;
 };
+
+//console.log(suitValue);
+//function BigFunc() {
+const numberArray = ["1", "2", "3", "4", "5"];
+const suitArray = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+
+function random(array) {
+  return Math.floor(Math.random() * array.length);
+}
+const numberValue = numberArray[random(numberArray)];
+const suitValue = suitArray[random(suitArray)];
+window.onload = function() {
+  document.querySelector(".number").innerHTML = numberValue;
+  document.querySelector(".top").innerHTML = suitValue;
+  document.querySelector(".bottom").innerHTML = suitValue;
+};
+//console.log("We are inside the if", suitValue);
+if (suitValue == "&hearts;" || suitValue == "&diams;") {
+  console.log("We are inside the if", suitValue);
+  //document.getElementsByClassName("top").style.color = "red";
+  const Element = document.getElementsByClassName("top");
+  Element[0].style.color = "red";
+  const Color = document.getElementsByClassName("bottom");
+  Color[0].style.color = "red";
+}
